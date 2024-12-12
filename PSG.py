63e1347190a2702e5be2cc9646dc0b69d3afeb14
Rudresh Singh <107204888+Ruudddiiii@users.noreply.github.com>
2024-12-12 18:52:12 +0530

    Update PSG.py

--- PSG.py
+++ PSG.py
@@ -2,14 +2,12 @@ import requests
 import json
 
 RAW_FILE_URL = 'https://raw.githubusercontent.com/Ruudddiiii/TaskTravelTime/main/task1.json'
-GITHUB_TOKEN = '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'
 
 def load_tasks_from_github():
-    headers = {'Authorization': f'token {GITHUB_TOKEN}'}
     try:
-        response = requests.get(RAW_FILE_URL, headers=headers)
-        response.raise_for_status()
-        data = response.json()  # Parse JSON content
+        response = requests.get(RAW_FILE_URL)
+        response.raise_for_status()  # Raise an error for bad HTTP status
+        data = response.json()  # Parse the JSON content
         return data.get('tasks', [])
     except requests.exceptions.RequestException as e:
         print(f"Error loading tasks: {e}")
